a61aea57f4a2fd86f2b8031826cdbab777d676e6
Reed W Priest <pries153@umn.edu>
2023-03-09 16:21:33 -0600

    Steps 1:7 done.

--- R/week7.R
+++ R/week7.R
@@ -11,4 +11,6 @@ week7_tbl <- read.csv("../data/week3.csv") %>%
   mutate(timeEnd = as.POSIXct(timeEnd)) %>%
   mutate(timeSpent = timeEnd - timeStart) %>%
   mutate(condition = str_replace_all(condition, c("B" = "Block B", "A" = "Block A", "C" = "Control"))) %>%
-  mutate(gender = str_replace_all(gender, c("F" = "Female", "M" = "Male")))
+  mutate(gender = str_replace_all(gender, c("F" = "Female", "M" = "Male"))) %>%
+  filter(q6 == 1) %>%
+  select(timeStart:q5,q7:timeSpent)
