241211a6ff667088c2d21874baba2206c63b7de3
Reed W Priest <pries153@umn.edu>
2023-03-09 22:56:59 -0600

    Steps 1:11 done.

--- R/week7.R
+++ R/week7.R
@@ -15,7 +15,8 @@ week7_tbl <- read.csv("../data/week3.csv") %>%
   filter(q6 == 1) %>%
   select(timeStart:q5,q7:timeSpent) %>%
   mutate(condition = as.factor(condition), gender = as.factor(gender)) %>%
-  mutate(q1 = as.numeric(q1), q2 = as.numeric(q2), q3 = as.numeric(q3), q4 = as.numeric(q4), q5 = as.numeric(q5), q7 = as.numeric(q7), q8 = as.numeric(q8), q9 = as.numeric(q9), q10 = as.numeric(q10)) 
+  mutate(q1 = as.numeric(q1), q2 = as.numeric(q2), q3 = as.numeric(q3), q4 = as.numeric(q4), q5 = as.numeric(q5), q7 = as.numeric(q7), q8 = as.numeric(q8), q9 = as.numeric(q9), q10 = as.numeric(q10)) %>%
+  mutate(gender = factor(gender, levels = c("Male", "Female")))
 
 
 # Visualization
@@ -23,8 +24,12 @@ ggpairs(data = week7_tbl, columns = 5:13, diag = list(continuous = "densityDiag"
 
 
 fig_1 <- (ggplot(data = week7_tbl, aes(x = timeStart, y = q1)) +
-  geom_point() +
-  labs(x = "Date of Experiment", y = "Q1 Score")) %>%
+            geom_point() +
+            labs(x = "Date of Experiment", y = "Q1 Score", )) %>%
   ggsave("../figs/fig1.png", .)
 
-
+fig_2 <- (ggplot(data = week7_tbl, aes(x = q1, y = q2, color = gender)) + 
+            geom_point()  +
+            guides(color = guide_legend(title = "Participant Gender")) +
+            geom_jitter()) %>%
+  ggsave("../figs/fig2.png", .)
