a08b7bd4fa52fa782faf172c6b2c123a10305283
Reed W Priest <pries153@umn.edu>
2023-03-13 12:29:02 -0500

    Post-class edits.

--- R/week7.R
+++ R/week7.R
@@ -25,32 +25,29 @@ ggpairs(data = week7_tbl, columns = 5:13, diag = list(continuous = "densityDiag"
 fig_1 <- (ggplot(data = week7_tbl, aes(x = timeStart, y = q1)) +
             geom_point() +
             labs(x = "Date of Experiment", y = "Q1 Score", )) %>%
-  ggsave("../figs/fig1.png", ., width = 5, height = 3)
+  ggsave("../figs/fig1.png", ., width = 1920, height = 1080, units = "px")
 
 
 fig_2 <- (ggplot(data = week7_tbl, aes(x = q1, y = q2, color = gender)) + 
-            geom_point()  +
-            guides(color = guide_legend(title = "Participant Gender")) +
-            geom_jitter()) %>%
-  ggsave("../figs/fig2.png", ., width = 10, height = 5)
+            geom_jitter()  +
+            guides(color = guide_legend(title = "Participant Gender"))) %>%
+  ggsave("../figs/fig2.png", ., width = 1920, height = 1080, units = "px")
 fig_3 <- (ggplot(data = week7_tbl, aes(x = q1, y = q2)) +
-  geom_point() +
-  facet_grid(cols = vars(gender)) + 
-  geom_jitter() +
-  labs(x = "Score on Q1", y = "Score on Q2")) %>%
-  ggsave("../figs/fig3.png", ., width = 10, height = 5)
+            geom_jitter() +
+            facet_grid(cols = vars(gender)) + 
+            labs(x = "Score on Q1", y = "Score on Q2")) %>%
+  ggsave("../figs/fig3.png", ., width = 1920, height = 1080, units = "px")
 
 fig_4 <- (ggplot(data = week7_tbl, aes(x = gender, y = timeSpent)) +
-  geom_boxplot() + 
-  labs(x = "Gender", y = "Time Elapsed (mins)")) %>%
-  ggsave("../figs/fig4.png", ., width = 10, height = 5)
+            geom_boxplot() + 
+            labs(x = "Gender", y = "Time Elapsed (mins)")) %>%
+  ggsave("../figs/fig4.png", ., width = 1920, height = 1080, units = "px")
 
 
 fig_5 <- (ggplot(data = week7_tbl, aes(x = q5, y = q7, color = condition)) +
-  geom_point() +
-  geom_jitter() +
-  labs(x = "Score on Q5", y = "Score on Q7") +
-  guides(color = guide_legend(title = "Experimental Condition")) +
-  theme(legend.position = "bottom", legend.background = element_rect(fill = "lightgray")) +
-  geom_smooth(method = "lm", se = FALSE)) %>%
-  ggsave("../figs/fig5.png", ., width = 10, height = 5)
+            geom_jitter(width = 0.1) +
+            labs(x = "Score on Q5", y = "Score on Q7") +
+            guides(color = guide_legend(title = "Experimental Condition")) +
+            theme(legend.position = "bottom", legend.background = element_rect(fill = "#e0e0e0")) +
+            geom_smooth(method = "lm", se = FALSE)) %>%
+  ggsave("../figs/fig5.png", ., width = 1920, height = 1080, units = "px")
